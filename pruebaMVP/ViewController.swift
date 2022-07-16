f7006ad7f64f31e8078f83559eb3c7b10c3039b6
MaryAngelFv <90918206+MaryAngelFv@users.noreply.github.com>
2022-07-16 15:09:33 -0500

    Initial Commit

---
+++ pruebaMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pruebaMVP
+//
+//  Created by Brayam Mora on 16/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
